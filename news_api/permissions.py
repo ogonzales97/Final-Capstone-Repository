c7ed302c3bcba6b25e8951fbf24a886ca16165a3
Orlando Gonzales <cppjrke2013@gmail.com>
2026-02-25 09:30:55 -0600

    Add docstrings to permissions.py

--- news_api/permissions.py
+++ news_api/permissions.py
@@ -4,23 +4,97 @@ from rest_framework import permissions
 
 
 class IsJournalist(permissions.BasePermission):
-    """Allows access only to journalists."""
+    """
+    Permission class that allows access only to journalists.
+    
+    Checks if the authenticated user has the is_journalist flag
+    set to True.
+    
+    :param request: The incoming HTTP request object
+    :type request: rest_framework.request.Request
+    :param view: The view being accessed
+    :type view: rest_framework.views.APIView
+    :return: True if user is a journalist, False otherwise
+    :rtype: bool
+    """
 
     def has_permission(self, request, view):
+        """
+        Check if the user is a journalist.
+        
+        :param request: The incoming HTTP request object
+        :type request: rest_framework.request.Request
+        :param view: The view being accessed
+        :type view: rest_framework.views.APIView
+        :return: True if user is authenticated and is a journalist
+        :rtype: bool
+        """
         return bool(request.user and request.user.is_journalist)
 
 
 class IsEditor(permissions.BasePermission):
-    """Allows access only to editors."""
+    """
+    Permission class that allows access only to editors.
+    
+    Checks if the authenticated user has the is_editor flag set to
+    True.
+    
+    :param request: The incoming HTTP request object
+    :type request: rest_framework.request.Request
+    :param view: The view being accessed
+    :type view: rest_framework.views.APIView
+    :return: True if user is an editor, False otherwise
+    :rtype: bool
+    """
 
     def has_permission(self, request, view):
+        """
+        Check if the user is an editor.
+        
+        :param request: The incoming HTTP request object
+        :type request: rest_framework.request.Request
+        :param view: The view being accessed
+        :type view: rest_framework.views.APIView
+        :return: True if user is authenticated and is an editor
+        :rtype: bool
+        """
         return bool(request.user and request.user.is_editor)
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """Journalists can edit their own work; others can only read."""
+    """
+    Permission class for object-level access control.
+    
+    Allows read-only access to everyone (GET, HEAD, OPTIONS requests).
+    Write access (PUT, PATCH, DELETE) is restricted to the
+    article author.
+    
+    :param request: The incoming HTTP request object
+    :type request: rest_framework.request.Request
+    :param view: The view being accessed
+    :type view: rest_framework.views.APIView
+    :param obj: The object being accessed
+    :type obj: news_api.models.Article
+    :return: True if user has permission, False otherwise
+    :rtype: bool
+    """
 
     def has_object_permission(self, request, view, obj):
+        """
+        Check if the user can access the specific object.
+        
+        Safe methods (GET, HEAD, OPTIONS) are allowed for everyone.
+        Write methods require the user to be the article author.
+        
+        :param request: The incoming HTTP request object
+        :type request: rest_framework.request.Request
+        :param view: The view being accessed
+        :type view: rest_framework.views.APIView
+        :param obj: The article object being accessed
+        :type obj: news_api.models.Article
+        :return: True if read-only or user is the author
+        :rtype: bool
+        """
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
